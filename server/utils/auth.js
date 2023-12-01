c5f946a3f7e37aff32763a8a546fab04185cbd22
gfuselier <gisellefuselier@gmail.com>
2023-12-01 13:30:45 -0600

    added dotenv package and env file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,8 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = process.env.SECRET;
+const expiration = process.env.EXPIRATION;
 
 module.exports = {
   AuthenticationError: new GraphQLError('Could not authenticate user.', {
