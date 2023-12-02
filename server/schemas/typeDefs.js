bbe1e943d070eb976b49f55681c6fd483c8efc19
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2023-12-02 08:39:01 -0600

    updated user address

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,6 +27,7 @@ const typeDefs = `
     firstName: String
     lastName: String
     email: String
+    address: String
     orders: [Order]
   }
 
@@ -62,7 +63,7 @@ const typeDefs = `
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     addOrder(products: [ID]!): Order
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateUser(firstName: String, lastName: String, email: String, password: String, address: String): User
     updateProduct(_id: ID!, quantity: Int!): Product
     login(email: String!, password: String!): Auth
   }
