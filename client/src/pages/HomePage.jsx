fa76cf8eb817a5064484170728740f88097a3afb
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-12-03 16:06:51 -0600

    fixed my side

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,46 +4,43 @@ import { FaInstagram, FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const main = () => {
   return (
-    <div className="bg-[#ddfff7] min-h-screen">
-      {/* Hero Section */}
-      <section className="bg-[#0e3b43] text-[#ddfff7] py-2">
-        <div className="container mx-auto text-center">
-          <h1 className="text-2xl font-bold mb-4 flex">Tech-E</h1>
-          <p className="text-xl mb-8">
-          Unleashing Tomorrow's Technology, Today — Explore a World of Innovation and Possibilities with Tech-E Solutions.
-          </p>
-     
+    <div className="bg-gray-100 min-h-screen">
+
+      <section className="bg-gray-100">
+        <div className="logo">
+          <img className='max-h-[200px] max-w-[200px]' src="../src/assets/images/Logo.webp" alt="Tech-E" />
         </div>
+
       </section>
 
-  
+
 
       {/* Product Showcase */}
       <section className="py-16">
         <div className="container mx-auto text-center">
-          <h2 className="text-3xl font-bold mb-8">Featured Products</h2>
+          <h2 className="text-3xl font-bold mb-8 ">Featured Products</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {/* Product Cards */}
             {/* You can create separate ProductCard components for each product */}
-            <div className="bg-[#93e1d8] p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-bold mb-2">Product 1</h3>
-              <p className="text-gray-700">Description of Product 1.</p>
+            <div className="bg-gray-100 border-[#ecd398] border-4 p-6 rounded-lg shadow-md">
+              <h3 className="text-xl font-bold mb-2 text-black">Product 1</h3>
+              <p className="text-black">Description of Product 1.</p>
             </div>
-            <div className="bg-[#93e1d8] p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-bold mb-2">Product 2</h3>
-              <p className="text-gray-700">Description of Product 2.</p>
+            <div className="bg-gray-100 border-[#ecd398] border-4 p-6 rounded-lg shadow-md">
+              <h3 className="text-xl font-bold mb-2 text-black">Product 2</h3>
+              <p className="text-black">Description of Product 2.</p>
             </div>
-            <div className="bg-[#93e1d8] p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-bold mb-2">Product 3</h3>
-              <p className="text-gray-700">Description of Product 3.</p>
+            <div className="bg-gray-100 border-[#ecd398] border-4 p-6 rounded-lg shadow-md">
+              <h3 className="text-xl font-bold mb-2 text-black">Product 3</h3>
+              <p className="text-black">Description of Product 3.</p>
             </div>
             {/* Add more products as needed */}
           </div>
         </div>
       </section>
 
-          {/* Social Media Links */}
-          <div className="bg-[#0e3b43] py-8">
+      {/* Social Media Links */}
+      <div className="bg-[#011638] py-8">
         <div className="container mx-auto flex justify-center">
           <a href="#" className="text-2xl mx-4 text-white hover:text-[#ecd398]">
             <FaInstagram />
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
+
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+console.log('CONN_STRING:', process.env.CONN_STRING);
+
 mongoose.connect(process.env.MONGODB_URI || process.env.CONN_STRING);
 
 module.exports = mongoose.connection;
