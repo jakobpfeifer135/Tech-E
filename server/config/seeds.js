c1fbe10b13e2cb11ef4892651e2eeaf6aa891433
gfuselier <gisellefuselier@gmail.com>
2023-12-03 21:47:27 -0600

    added test user and require dotenv config

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -72,21 +72,9 @@ db.once('open', async () => {
   console.log('products seeded');
 
   await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
-    password: 'password12345',
-    orders: [
-      {
-        products: [products[0]._id, products[0]._id, products[1]._id]
-      }
-    ]
-  });
-
-  await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'test@testmail.com',
     password: 'password12345'
   });
 
