6e4e0a73ddc8684c32175f3f5801ea52c7cb6213
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-12-04 09:52:02 -0600

    pushing quickfix for main and app.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+// main.jsx
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Checkout from './pages/Checkout.jsx';
+import HomePage from './pages/HomePage.jsx';
+import Login from './pages/Login.jsx';
+
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/Checkout',
+        element: <Checkout />
+      },
+      {
+        path: '/Login',
+        element: <Login />
+      },
+ 
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <RouterProvider router={router} />
 );
 
