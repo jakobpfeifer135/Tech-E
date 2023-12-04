e003c2c0e8e2adf46b4430702010452ecc8afd61
gfuselier <gisellefuselier@gmail.com>
2023-12-03 22:04:45 -0600

    hardcoded connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 console.log('MONGODB_URI:', process.env.MONGODB_URI);
-console.log('CONN_STRING:', process.env.CONN_STRING);
+// console.log('CONN_STRING:', process.env.CONN_STRING);
 
-mongoose.connect(process.env.MONGODB_URI || process.env.CONN_STRING);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech_e_db');
 
 module.exports = mongoose.connection;
