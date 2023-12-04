0f205f893bd46a57d766489bee8a4af89cb6be3a
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-12-04 14:41:41 -0600

    added more homepage styles

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,39 +3,40 @@ import { Link } from 'react-router-dom';
 
 const ProductCard = ({ product }) => {
   const { id, name, description, imageUrl, colorOptions, price } = product;
-  const [selectedColor, setSelectedColor] = useState(colorOptions[0]);
+  const [selectedColor, setSelectedColor] = useState(colorOptions[3]);
 
   const handleColorSelection = (color) => {
     setSelectedColor(color);
   };
 
   return (
-    <div key={id} className="bg-gray-100 border-[--Gold] border-4 p-6 rounded-lg shadow-md">
-      <div className="mb-4">
+    <div key={id} className="bg-[--Navy] p-6  rounded-lg shadow-md">
+      <div className="mb-4" style={{ marginTop: '-50px' }}>
         <img
-          className="w-full h-32 object-cover object-center rounded-md"
+          className="border-l-8 border-r-8 border-b-8 border-gray-100 w-full h-36 object-cover object-center rounded-xl"
           src={imageUrl[colorOptions.indexOf(selectedColor)]}
           alt={`Product ${id}`}
         />
       </div>
-      <h3 className="text-xl font-bold mb-2 text-black">{name}</h3>
-      <p className="text-black">{description}</p>
-      <p className="text-black font-semibold">Price: ${price}</p>
-      <div className="mt-4 flex space-x-2">
+      <h3 className="text-lg text-center font-bold mb-2 text-white">{name}</h3>
+      <p className="text-white pb-3">{description}</p>
+      <p className="text-white text-center font-semibold">Price: ${price}</p>
+      <div className="mt-4 flex flex-wrap p-2 justify-center items-center "> {/* Updated: Added flex-wrap */}
         {colorOptions.map((color) => (
           <button
             key={color}
             onClick={() => handleColorSelection(color)}
-            className={`w-6 h-6 rounded-full border border-gray-500 ${selectedColor === color ? 'border-black' : ''}`}
-            style={{ backgroundColor: color }}
-          ></button>
+            className={`bg-white text-black p-2 mr-2 rounded-md hover:bg-[--Gold] hover:text-black font-serif mb-2 min-w-[55px] max-w-[55px]`} 
+          >
+            {color}
+          </button>
         ))}
       </div>
       <Link
         to={`/product/${id}`} // Redirect to the product details page
-        className="block mt-4 bg-black text-white px-4 py-2 rounded-md hover:bg-[--Gold] hover:text-black focus:outline-none focus:ring focus:border-indigo-300 font-serif"
+        className="block mt-4 bg-black text-white px-4 py-2 rounded-md hover:bg-[--Gold] hover:text-black   font-serif text-center"
       >
-        View Details
+       Add To Cart
       </Link>
     </div>
   );
