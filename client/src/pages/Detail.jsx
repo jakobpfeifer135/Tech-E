00afd4c88bdd182504a714877264ae7bd8498cb5
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-12-06 21:50:35 -0600

    products detail

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
 import Cart from '../components/Cart';
 import { useStoreContext } from '../utils/GlobalState';
 import {
@@ -19,29 +18,21 @@ function Detail() {
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
-
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-
   const { products, cart } = state;
 
   useEffect(() => {
-    // already in global store
     if (products.length) {
       setCurrentProduct(products.find((product) => product._id === id));
-    }
-    // retrieved from server
-    else if (data) {
+    } else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
-
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
-    }
-    // get cache from idb
-    else if (!loading) {
+    } else if (!loading) {
       idbPromise('products', 'get').then((indexedProducts) => {
         dispatch({
           type: UPDATE_PRODUCTS,
@@ -77,38 +68,60 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id,
     });
-
     idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   return (
     <>
       {currentProduct && cart ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+        <div className="container mx-auto mt-10">
+          <Link to="/" className="text-indigo-600 hover:text-indigo-500">
+            Go Back
+          </Link>
 
-          <h2>{currentProduct.name}</h2>
+          <div className="flex flex-col md:flex-row mt-8">
+            <div className="md:w-1/2">
+              <img
+                src={`${currentProduct.image}`}
+                alt={currentProduct.name}
+                className="max-w-full"
+              />
+            </div>
 
-          <p>{currentProduct.description}</p>
+            <div className="md:w-1/2 md:ml-8">
+              <h2 className="text-3xl font-bold mb-4">{currentProduct.name}</h2>
+              <p className="text-gray-700 mb-6">{currentProduct.description}</p>
 
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button onClick={addToCart}>Add to Cart</button>
-            <button
-              disabled={!cart.find((p) => p._id === currentProduct._id)}
-              onClick={removeFromCart}
-            >
-              Remove from Cart
-            </button>
-          </p>
+              <p className="text-lg text-gray-900">
+                <strong>Price:</strong> ${currentProduct.price}
+              </p>
 
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
+              <div className="mt-4 flex">
+                <button
+                  className="bg-indigo-500 text-white px-4 py-2 rounded-md mr-4"
+                  onClick={addToCart}
+                >
+                  Add to Cart
+                </button>
+                <button
+                  className="text-indigo-500 hover:text-indigo-400"
+                  disabled={!cart.find((p) => p._id === currentProduct._id)}
+                  onClick={removeFromCart}
+                >
+                  Remove from Cart
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+
+      {loading ? (
+        <div className="flex justify-center items-center mt-10">
+          <img src={spinner} alt="loading" />
+        </div>
+      ) : null}
+
       <Cart />
     </>
   );
